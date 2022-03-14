cf669bfabf45f009fb0bedf755e761107fd74c63
Rahul Sharma <holdmypotion@gmail.com>
2022-03-15 00:05:13 +0530

    Setup generic error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import "express-async-errors";
 import { indexRouter } from "./routes";
 import { signinRouter } from "./routes/signin";
 import { createRouter } from "./routes/new";
 import { updateRouter } from "./routes/update";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -15,8 +18,10 @@ app.use(signinRouter);
 app.use(createRouter);
 app.use(updateRouter);
 
-app.all("*", async (req, res) => {
-  res.send("nothing");
+app.all("*", async () => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 export { app };
--- server/src/routes/signin.ts
+++ server/src/routes/signin.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
-router.get("/api/interviews", async (req: Request, res: Response) => {
-  res.send("<h2> List Interviews <h2/>");
-});
+router.post(
+  "/api/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    // const { email, password } = req.body;
+    console.log("Creating a user...");
+  }
+);
 
 export { router as signinRouter };
