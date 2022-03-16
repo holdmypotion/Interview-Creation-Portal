393acc79a0d516cc1673b6b93f12135e41fa8fec
Rahul Sharma <holdmypotion@gmail.com>
2022-03-16 13:38:42 +0530

    adding domain

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ export default function buildClient({ req }) {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://holdmypotion.tech",
       headers: req.headers,
     });
   } else {
@@ -16,3 +15,5 @@ export default function buildClient({ req }) {
     });
   }
 }
+
+// "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
