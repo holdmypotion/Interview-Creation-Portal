1519a9b3bebcf3b0fa0723447956dac12af4729d
Rahul Sharma <holdmypotion@gmail.com>
2022-03-16 09:43:39 +0530

    full updates

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,13 +1,25 @@
 import axios from "axios";
 import { useState } from "react";
 
-export default ({ url, method, body, onSuccess }) => {
+export default ({ url, method, body, onSuccess, fileUpload }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async (props = {}) => {
     try {
       setErrors(null);
-      const response = await axios[method](url, { ...body, ...props });
+      if (fileUpload) {
+        const formData = new FormData();
+        for (let [key, val] of body) {
+          formData.append(key, val);
+        }
+        const response = await axios[method](url, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+      } else {
+        const response = await axios[method](url, { ...body, ...props });
+      }
 
       if (onSuccess) {
         onSuccess(response.data);
