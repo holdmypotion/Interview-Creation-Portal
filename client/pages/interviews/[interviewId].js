74009d55b991cca364e8e3f9b1dcde56bbf4859a
Rahul Sharma <holdmypotion@gmail.com>
2022-03-17 08:59:14 +0530

    Frontend improvements

--- client/pages/interviews/[interviewId].js
+++ client/pages/interviews/[interviewId].js
@@ -1,10 +1,14 @@
 import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 import Link from "next/link";
 
-const InterviewShow = ({ interview, participantsList }) => {
+const InterviewShow = ({ currentUser, interview, participantsList }) => {
+  useEffect(() => {
+    if (!currentUser || interview.length <= 0) Router.push("/auth/signup");
+  }, [currentUser]);
+
   const [startTime, setStartTime] = useState(new Date(interview.startTime));
   const [endTime, setEndTime] = useState(new Date(interview.endTime));
   const [participants, setParticipants] = useState(interview.participants);
@@ -49,112 +53,122 @@ const InterviewShow = ({ interview, participantsList }) => {
 
   console.log(participants);
 
-  const participantsJSX = participantsList.map(participant => {
-    return (
-      <tr key={participant.email}>
-        <th>
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-            checked={checkIfPresent(participant.email)}
-            onChange={e => checkboxHandler(e, participant.email)}
-          />
-        </th>
-        <td>{participant.name}</td>
-        <td>{participant.email}</td>
-        <td>
-          {participant.interviews &&
-            participant.interviews.map(interview => {
-              const formattedStartTime = new Date(interview.startTime)
-                .toString()
-                .substring(0, 21);
-              const formattedEndTime = new Date(interview.endTime)
-                .toString()
-                .substring(0, 21);
-              return (
-                <div key={interview.startTime}>
-                  {formattedStartTime} - {formattedEndTime}
-                </div>
-              );
-            })}
-        </td>
-      </tr>
-    );
-  });
+  let participantsJSX;
+  if (participantsList && participantsList.length > 0) {
+    participantsJSX = participantsList.map(participant => {
+      return (
+        <tr key={participant.email}>
+          <th>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+              checked={checkIfPresent(participant.email)}
+              onChange={e => checkboxHandler(e, participant.email)}
+            />
+          </th>
+          <td>{participant.name}</td>
+          <td>{participant.email}</td>
+          <td>
+            {participant.interviews &&
+              participant.interviews.map(interview => {
+                const formattedStartTime = new Date(interview.startTime)
+                  .toString()
+                  .substring(0, 21);
+                const formattedEndTime = new Date(interview.endTime)
+                  .toString()
+                  .substring(0, 21);
+                return (
+                  <div key={interview.startTime}>
+                    {formattedStartTime} - {formattedEndTime}
+                  </div>
+                );
+              })}
+          </td>
+        </tr>
+      );
+    });
+  }
 
   const formattedStartTime = new Date();
   formattedStartTime.setMinutes(formattedStartTime.getMinutes() + 30);
   formattedStartTime.setMinutes(0, 0, 0);
 
   return (
-    <form onSubmit={onSubmit}>
-      <div className="d-flex justify-content-center py-3">
-        <h1>Update Interview</h1>
-      </div>
-      <div className="d-flex justify-content-center pt-3">
-        <h5>Interviewees List</h5>
-      </div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Booked Slots</th>
-          </tr>
-        </thead>
-        <tbody>{participantsJSX}</tbody>
-      </table>
-      <div className="d-flex justify-content-center">
-        <Link href="/participant" as="/participant">
-          <a>
-            <button className="btn btn-primary">Add Interviewee</button>
-          </a>
-        </Link>
-      </div>
-      <div className="d-flex justify-content-center pt-5">
-        <div>
-          <div className="pt-3 pd-3">
-            <span>Start Time</span>
-            <DateTimePickerComponent
-              id="datetimepicker"
-              min={formattedStartTime}
-              placeholder="From"
-              format="dd-MMM-yy HH:mm"
-              onChange={e => setStartTime(e.value)}
-              value={new Date(interview.startTime)}
-            />
+    <>
+      {currentUser && (
+        <form onSubmit={onSubmit}>
+          <div className="d-flex justify-content-center py-3">
+            <h1>Update Interview</h1>
           </div>
-          <div className="pt-3 pd-3">
-            <span>End Time</span>
+          <div className="d-flex justify-content-center pt-3">
+            <h5>Interviewees List</h5>
+          </div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Booked Slots</th>
+              </tr>
+            </thead>
+            <tbody>{participantsJSX}</tbody>
+          </table>
+          <div className="d-flex justify-content-center">
+            <Link href="/participant" as="/participant">
+              <a>
+                <button className="btn btn-primary">Add Interviewee</button>
+              </a>
+            </Link>
+          </div>
+          <div className="d-flex justify-content-center pt-5">
+            <div>
+              <div className="pt-3 pd-3">
+                <span>Start Time</span>
+                <DateTimePickerComponent
+                  id="datetimepicker"
+                  min={formattedStartTime}
+                  placeholder="From"
+                  format="dd-MMM-yy HH:mm"
+                  onChange={e => setStartTime(e.value)}
+                  value={new Date(interview.startTime)}
+                />
+              </div>
+              <div className="pt-3 pd-3">
+                <span>End Time</span>
 
-            <DateTimePickerComponent
-              id="datetimepicker"
-              min={startTime}
-              placeholder="To"
-              format="dd-MMM-yy HH:mm"
-              onChange={e => setEndTime(e.value)}
-              value={new Date(interview.endTime)}
-            />
+                <DateTimePickerComponent
+                  id="datetimepicker"
+                  min={startTime}
+                  placeholder="To"
+                  format="dd-MMM-yy HH:mm"
+                  onChange={e => setEndTime(e.value)}
+                  value={new Date(interview.endTime)}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      {errors}
-      <div className="d-flex justify-content-center pt-5">
-        <button className="btn btn-primary">Submit</button>
-      </div>
-    </form>
+          {errors}
+          <div className="d-flex justify-content-center pt-5">
+            <button className="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
-InterviewShow.getInitialProps = async (context, client) => {
-  const { interviewId } = context.query;
-  const { data } = await client.get(`/api/interviews/${interviewId}`);
-  const participantsList = await client.get("/api/participants");
+InterviewShow.getInitialProps = async (context, client, currentUser) => {
+  if (currentUser) {
+    const { interviewId } = context.query;
+    const { data } = await client.get(`/api/interviews/${interviewId}`);
+    const participantsList = await client.get("/api/participants");
 
-  return { interview: data, participantsList: participantsList.data };
+    return { interview: data, participantsList: participantsList.data };
+  }
+  return { interview: {}, participantsList: {} };
 };
 
 export default InterviewShow;
--- client/pages/participant.js
+++ client/pages/participant.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Router from "next/router";
 import useRequest from "../hooks/use-request";
 
-export default function AddInterviewee() {
+export default function AddInterviewee({ currentUser }) {
+  useEffect(() => {
+    if (!currentUser) Router.push("/auth/signup");
+  }, [currentUser]);
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [file, setFile] = useState();
@@ -28,43 +32,47 @@ export default function AddInterviewee() {
   // console.log(file);
 
   return (
-    <div className="d-flex justify-content-center pt-3">
-      <form onSubmit={onSubmit} encType="multipart/form-data">
-        <h1>Add Interviewee</h1>
-        <div className="form-group">
-          <label>Name</label>
-          <input
-            value={name}
-            onChange={e => setName(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label>Email Address</label>
-          <input
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        {/* <div className="form-group">
-        <label htmlFor="formFile" className="form-label">
-          Resume
-          </label>
-        <input
-        className="form-control"
-          type="file"
-          id="formFile"
-          name="file"
-          placeholder="Select file"
-          onChange={e => setFile(e.target.files[0])}
-        />
-      </div> */}
-        {errors}
+    <>
+      {currentUser && (
         <div className="d-flex justify-content-center pt-3">
-          <button className="btn btn-primary">Add</button>
+          <form onSubmit={onSubmit} encType="multipart/form-data">
+            <h1>Add Interviewee</h1>
+            <div className="form-group">
+              <label>Name</label>
+              <input
+                value={name}
+                onChange={e => setName(e.target.value)}
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Email Address</label>
+              <input
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                className="form-control"
+              />
+            </div>
+            {/* <div className="form-group">
+          <label htmlFor="formFile" className="form-label">
+            Resume
+            </label>
+          <input
+          className="form-control"
+            type="file"
+            id="formFile"
+            name="file"
+            placeholder="Select file"
+            onChange={e => setFile(e.target.files[0])}
+          />
+        </div> */}
+            {errors}
+            <div className="d-flex justify-content-center pt-3">
+              <button className="btn btn-primary">Add</button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      )}
+    </>
   );
 }
